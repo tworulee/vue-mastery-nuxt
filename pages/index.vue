5b82e125acbae53081a4b54544978cee11cfe715
Tuğrul Yüce <tugrulyuce80@gmail.com>
2023-11-11 17:31:29 +0300

    fetching-creating api

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Home Page for Nuxt 3 server Course</h1>
+    <ul>
+        <li>
+            <img :src="scizor.sprite">
+        </li>
+        <li>
+           {{ scizor.id }} : {{ scizor.name }} 
+        </li>
+    </ul>
+</template>
+
+<script setup>
+const {data:scizor} = await useFetch("/api/bulbasaur")
+
+</script>
+
+<style>
+
+</style>
---
+++ server/api/[pokemon].ts
@@ -0,0 +1,12 @@
+export default defineEventHandler(async event => {
+    const {pokemon} = event.context.params
+    const response = await $fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+
+    const idealResponse= {
+        id:response.id,
+        name:response.name,
+        sprite:response.sprites.front_default
+    }
+
+    return idealResponse
+})
